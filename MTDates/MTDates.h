9a5a17d2b9240b5555462a814bfaccde91d4f3c3
Sergey Zenchenko <sergeizenchenko@gmail.com>
2016-06-21 13:32:03 +0300

    Add carthage support.

---
+++ MTDates/MTDates.h
@@ -0,0 +1,22 @@
+//
+//  MTDates.h
+//  MTDates
+//
+//  Created by Sergey Zenchenko on 6/21/16.
+//  Copyright © 2016 Mysterious Trousers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTDates.
+FOUNDATION_EXPORT double MTDatesVersionNumber;
+
+//! Project version string for MTDates.
+FOUNDATION_EXPORT const unsigned char MTDatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTDates/PublicHeader.h>
+
+#import <MTDates/NSDate+MTDates.h>
+#import <MTDates/NSDateComponents+MTDates.h>
+
+
